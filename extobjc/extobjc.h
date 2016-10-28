0d2ad15458c8d10c1be8cca8c8b077fa8aa8e8ba
李瑞勤 <ruiqin.li@eascs.com>
2016-10-29 01:14:41 +0800

    添加framework target

--- extobjc/extobjc.h
+++ extobjc/extobjc.h
@@ -1,20 +1,20 @@
 //
-//  extobjc.h
-//  extobjc
+//  Extobjc.h
+//  Extobjc
 //
-//  Created by Justin Spahr-Summers on 2010-11-09.
-//  Copyright (C) 2012 Justin Spahr-Summers.
-//  Released under the MIT license.
+//  Created by Chapter Zhang on 16/10/29.
+//
 //
 
-#import "EXTADT.h"
-#import "EXTConcreteProtocol.h"
-#import "EXTKeyPathCoding.h"
-#import "EXTNil.h"
-#import "EXTSafeCategory.h"
-#import "EXTScope.h"
-#import "EXTSelectorChecking.h"
-#import "EXTSynthesize.h"
-#import "NSInvocation+EXT.h"
-#import "NSMethodSignature+EXT.h"
+#import <UIKit/UIKit.h>
+#import "metamacros.h"
+
+//! Project version number for Extobjc.
+FOUNDATION_EXPORT double ExtobjcVersionNumber;
+
+//! Project version string for Extobjc.
+FOUNDATION_EXPORT const unsigned char ExtobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extobjc/PublicHeader.h>
+
 
